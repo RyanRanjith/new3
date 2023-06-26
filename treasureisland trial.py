d4e11d574f70b9dcf88d6890a8f0340f85a0fd5e
RyanRanjith <kingranjith4@gmail.com>
2023-06-26 09:42:53 -0700

    project2

---
+++ treasureisland trial.py
@@ -0,0 +1,52 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+riddle1=input("you are at a crossroad where you should go left or right?")
+change1= riddle1.lower()
+if change1 == "left":
+   riddle2 = input(f"you came to a lake type wait or swim")
+elif change1 == "right":
+    print("Game Over")
+    
+    
+change2 = riddle2.lower()
+if change2 == "wait":
+    riddle3 = input("choose yellow, red or blue door")
+elif change2 == "swim":
+    print("you are dead")
+    
+    
+change3 = riddle3.lower()
+
+if change3 == "yellow":
+    print("win")
+elif change3 =="red":
+    print("lose")
+elif change3 == "blue":
+    print("fall")
